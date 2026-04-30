bf2b0c37d5f9d2665209b526ff4257a4c2093bcf
RezerOfgit <qingcaicoke@163.com>
2026-04-30 15:31:25 +0800

    feat: integrate Knife4j to generate interactive API documentation

---
+++ src/main/java/com/dcp/config/Knife4jConfig.java
@@ -0,0 +1,70 @@
+package com.dcp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Re-zero
+ * @version 1.0
+ */
+@Configuration
+@EnableSwagger2
+public class Knife4jConfig {
+
+    @Bean
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("数字化耗材管控平台 (DCP) - API 文档")
+                        .description("提供高并发耗材管理、领用审批、权限校验的后端接口支撑。")
+                        .contact(new Contact("你的名字", "https://github.com/your-link", "your-email@qq.com"))
+                        .version("V1.0")
+                        .build())
+                .select()
+                // 指定要扫描的 Controller 包路径 (改成你的包名)
+                .apis(RequestHandlerSelectors.basePackage("com.dcp.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                // 开启全局 Token 支持
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
+    }
+
+    // 设置请求头中需要带 Authorization
+    private List<SecurityScheme> securitySchemes() {
+        List<SecurityScheme> apiKeyList = new ArrayList<>();
+        apiKeyList.add(new ApiKey("Authorization", "Authorization", "header"));
+        return apiKeyList;
+    }
+
+    // 设置需要携带 Token 的全局路径（这里设置除了登录之外的所有接口）
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> securityContexts = new ArrayList<>();
+        securityContexts.add(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build());
+        return securityContexts;
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List<SecurityReference> securityReferences = new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization", authorizationScopes));
+        return securityReferences;
+    }
+}
--- src/main/java/com/dcp/controller/MaterialController.java
+++ src/main/java/com/dcp/controller/MaterialController.java
@@ -3,6 +3,8 @@ package com.dcp.controller;
 import com.dcp.dto.R;
 import com.dcp.entity.Material;
 import com.dcp.service.MaterialService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,18 +15,21 @@ import java.util.List;
  * @author Re-zero
  * @version 1.0
  */
+@Api(tags = "📦 耗材账目管理模块") // 【新增】给整个 Controller 命名
 @RestController
 @RequestMapping("/api/material")
 public class MaterialController {
     @Resource
     private MaterialService materialService;
 
+    @ApiOperation("查询耗材列表 (无权限限制)") // 【新增】给具体的接口命名
     @GetMapping("/list")
     public R<List<Material>> list() {
         return R.ok(materialService.listAll());
     }
 
     // 真正的耗材入库接口：只有 ADMIN 角色才能访问！
+    @ApiOperation("耗材入库 (仅限 ADMIN)") // 【新增】给具体的接口命名
     @PostMapping("/add")
     @PreAuthorize("hasRole('ADMIN')")
     public R<Void> add(@RequestBody Material material) {
